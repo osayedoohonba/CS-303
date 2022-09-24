19936bbcdf508c1a0ada82317565e764aa66f04a
Osay Edo-Ohonba <>
2022-09-24 02:52:39 +0000

    Initial Commit

---
+++ Functions.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+//constant integer for easier accses to size variable
+const int size = 200;
+
+void printArray(int* array, int size);
+void modInt(int* array, int size, int index, int newVal);
+int checkIfExsits(int* array, int size, int number);
+void addNewInt(int* array, int size, int number);
+void removeInt(int* array, int size, int index);
